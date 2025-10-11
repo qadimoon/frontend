75748613ef0f1bbb3f4b6377e6f6c7b3e7c1bde4
Ahmad Majid Abbas <ahmadmajidabbas@Ahmads-MacBook-Air.local>
2025-10-12 00:52:31 +0300

    Fix budget size error

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./common/components/header/header";
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
-    Header,
-    
+    RouterOutlet,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
